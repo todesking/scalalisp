f75d350a844436d13b200864e32a0caea5a0ad7c
todesking <discommunicative@gmail.com>
2014-08-30 18:33:21 +0900

    closure

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,19 @@
 package com.todesking.scalalisp
 
+object Tapper {
+  implicit class Tap[A](obj:A) {
+    def tap(f:A=>Unit):A = { f(obj); obj }
+  }
+}
+import Tapper._
+
 object Main {
   implicit class ToS(value:Any) {
     def toS:S = U.parse(value)
   }
   implicit class SProcess(ve:(S, Env)) {
     def >>(expr:S):(S, Env) = ve._2.eval(expr)
-    def ===(expected:S):Unit = assert(ve._1 == expected)
+    def ===(expected:S):Unit = assert(ve._1 == expected, s"expected ${expected} but ${ve._1}")
   }
   def l(values:Any*) = U.parse(values)
   def main(args:Array[String]):Unit = {
@@ -21,12 +28,36 @@ object Main {
       l('lambda, l('x),
         l('+, 'x, 1)),
       100)) === Num(101)
+
+    env.eval(l(Sym("set!"), 'plus,
+        l('lambda, l('x, 'y),
+          l('+, 'x, 'y)) )) >>
+      l(Sym("set!"), 'foo, 100) >>
+      l('plus, 'foo, 10) === Num(110)
+
+    env.eval(l(
+      l('lambda, l('x, 'y),
+        l(l('lambda, l('x), l('+, 'x, 'y)),
+          l('+, 'x, 1), 'y)),
+      10, 20)) === Num(31)
+
+    env.eval(
+      l(Sym("set!"), 'incr,
+        l(
+          l('lambda, l('counter),
+            l('lambda, l(), l('begin,
+              l(Sym("set!"), 'counter, l('+, 'counter, 1)),
+              'counter))),
+          0))) >>
+      l('incr) >>
+      l('incr) === Num(2)
   }
 }
 
 class Env(values:Map[String, S]) {
   def this(values:(String, S)*) = this(Map(values:_*))
   def eval(expr:S):(S, Env) = {
+    println(s"EVAL ${expr}, ENV=${this}")
     expr match {
       case Sym(name) => (values.getOrElse(name, throw new E(s"Symbol not found: ${name}")), this)
       case Num(n) => (Num(n), this)
@@ -47,27 +78,32 @@ class Env(values:Map[String, S]) {
       Some((SNil, e.update(name, value)))
     case (Sym("lambda"), Seq(ProperList(args @ _*), body:S)) =>
       Some(
-        Lambda(args.map{case Sym(name) => name case _ => throw new E("argument should symbol")}, body),
+        Lambda(this, args.map{case Sym(name) => name case _ => throw new E("argument should symbol")}, body),
         this
       )
+    case (Sym("begin"), body) => Some(
+      body.foldLeft[(S, Env)]((SNil, this)) { (re, expr) => val (_, env) = re; env.eval(expr) }
+    )
     case _ => None
   }
   def evalApply(func:S, args:Seq[S]):(S, Env) = {
     val (evaledArgs, env) = evalSeq(args)
     env.eval(func) match {
       case (NativeProc(body), e) => (body(evaledArgs), e)
-      case (Lambda(params, body), e) =>
-        extend(params, evaledArgs).eval(body)
+      case (lambda @ Lambda(closed, params, body), e) =>
+        closed.extend(params, evaledArgs).eval(body) match {
+          case (result, closed) => lambda.env = closed; (result, e)
+        }
       case unk => throw new RuntimeException(s"cant apply to ${unk}")
     }
   }
 
-  def evalSeq(exprs:Seq[S]):(Seq[S], Env) = exprs.foldLeft((Seq[S](), this)) { (re, expr) => val (results, env) = re; val (result, newe) = env.eval(expr); (result +: results, newe) }
+  def evalSeq(exprs:Seq[S]):(Seq[S], Env) = exprs.foldLeft((Seq[S](), this)) { (re, expr) => val (results, env) = re; val (result, newe) = env.eval(expr); (results :+ result, newe) }
 
   def update(name:String, value:S):Env = new Env(values + (name -> value))
 
-  def extend(params:Seq[String], args:Seq[S]) = new Env(
-    params.zip(args).foldLeft(values) { (values, pa) => val (param, arg) = pa; values + (param -> arg) }
+  def extend(names:Seq[String], values:Seq[S]) = new Env(
+    names.zip(values).foldLeft(this.values) { (vs, nv) => val (n, v) = nv; vs + (n -> v) }
   )
 }
 
@@ -104,7 +140,7 @@ case object SNil                       extends S {
 case class Num(value:Int)               extends S
 case class Sym(value:String)            extends S
 case class NativeProc(body:Seq[S] => S) extends S
-case class Lambda(args:Seq[String], body:S) extends S
+case class Lambda(var env:Env, args:Seq[String], body:S) extends S
 
 object ProperList {
   def unapplySeq(value:S):Option[Seq[S]] = value match {
