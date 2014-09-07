dda152b7b643011734f4dc63952dc70396bd02c3
todesking <discommunicative@gmail.com>
2014-09-07 23:49:02 +0900

    Make Env to mutable

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,102 +1,109 @@
 package com.todesking.scalalisp
 
-object Main {
-  implicit class ToS(value:Any) {
-    def toS:S = U.parse(value)
+object Extensions {
+  implicit class Opt[A](self:A) {
+    def toSome = Some(self)
   }
-  implicit class SProcess(ve:(S, Env)) {
-    def >>(expr:S):(S, Env) = ve._2.eval(expr)
-    def ===(expected:S):Unit = assert(ve._1 == expected, s"expected ${expected} but ${ve._1}")
+}
+import Extensions._
+
+object Main {
+  implicit class SProcess(self:S) {
+    def ===(expected:S):Unit = assert(self == expected, s"expected ${expected} but ${self}")
   }
   def l(values:Any*) = U.parse(values)
   def main(args:Array[String]):Unit = {
     val set_! = Sym("set!")
 
-    val env = Env.newGlobal()
+    def eval(ss:S*):S = {
+      val env = Env.newGlobal()
+      ss.foldLeft[S](SNil){(_, s) => env.eval(s)}
+    }
 
-    env.eval(Num(1)) === Num(1)
-    env.eval(l('+, 1, 2)) === Num(3)
-    env.eval(l('if, 1, 2, 3)) === Num(2)
-    env.eval(l('if, SNil, 2, 3)) === Num(3)
-    env.eval(l(set_!, 'x, 1)) >> 'x.toS === Num(1)
-    env.eval(l(
+    eval(S(1))                  === S(1)
+    eval(l('+, 1, 2))             === S(3)
+    eval(l('if, 1, 2, 3))         === S(2)
+    eval(l('if, SNil, 2, 3))      === S(3)
+    eval(l(set_!, 'x, 1), S('x)) === S(1)
+    eval(l(
       l('lambda, l('x),
         l('+, 'x, 1)),
-      100)) === Num(101)
+      100)) === S(101)
 
-    env.eval(l(set_!, 'plus,
+    eval(l(set_!, 'plus,
         l('lambda, l('x, 'y),
-          l('+, 'x, 'y)) )) >>
-      l(set_!, 'foo, 100) >>
-      l('plus, 'foo, 10) === Num(110)
+          l('+, 'x, 'y)) ),
+      l(set_!, 'foo, 100),
+      l('plus, 'foo, 10)) === S(110)
 
-    env.eval(l(
+    eval(l(
       l('lambda, l('x, 'y),
         l(l('lambda, l('x), l('+, 'x, 'y)),
           l('+, 'x, 1), 'y)),
-      10, 20)) === Num(31)
+      10, 20)) === S(31)
 
-    env.eval(
+    eval(
       l(set_!, 'incr,
         l(
           l('lambda, l('counter),
             l('lambda, l(), l('begin,
               l(set_!, 'counter, l('+, 'counter, 1)),
               'counter))),
-          0))) >>
-      l('incr) >>
-      l('incr) === Num(2)
+          0)),
+      l('incr),
+      l('incr)) === S(2)
   }
 }
 
-class Env(values:Map[String, S]) {
-  def this(values:(String, S)*) = this(Map(values:_*))
+class Env(var values:Map[String, S]) {
+
+  def lookup(key:String):S =
+    values.getOrElse(key, throw new E(s"Symbol not found: ${key}"))
+
   override def toString = values.toString
-  def eval(expr:S):(S, Env) = {
+
+  def eval(expr:S):S = {
     println(s"EVAL ${expr}, ENV=${this}")
     expr match {
-      case Sym(name) => (values.getOrElse(name, throw new E(s"Symbol not found: ${name}")), this)
-      case Num(n) => (Num(n), this)
+      case Sym(name) => lookup(name)
+      case n @ Num(_) => n
+      case SNil => SNil
       case ProperList(func, args @ _*) =>
         evalSpecialO(func, args) getOrElse evalApply(func, args)
-      case SNil => (SNil, this)
       case _ => throw new E(s"Unsupported expr: ${expr}")
     }
   }
 
-  def evalSpecialO(id:S, args:Seq[S]):Option[(S, Env)] = (id, args) match {
+  def evalSpecialO(id:S, args:Seq[S]):Option[S] = (id, args) match {
     case (Sym("if"), Seq(condS, thenS, elseS)) =>
-      val (cond, e) = eval(condS)
-      if(cond.isTruthy) Some(e.eval(thenS))
-      else              Some(e.eval(elseS))
+      val cond = eval(condS)
+      val body = if(cond.isTruthy) thenS else elseS
+      eval(body).toSome
     case (Sym("set!"), Seq(Sym(name), valueS)) =>
-      val (value, e) = eval(valueS)
-      Some((SNil, e.update(name, value)))
+      update(name, eval(valueS))
+      Some(SNil)
     case (Sym("lambda"), Seq(ProperList(args @ _*), body:S)) =>
-      Some(
-        Lambda(this, args.map{case Sym(name) => name case _ => throw new E("argument should symbol")}, body),
-        this
-      )
-    case (Sym("begin"), body) => Some(
-      body.foldLeft[(S, Env)]((SNil, this)) { (re, expr) => val (_, env) = re; env.eval(expr) }
-    )
+      Lambda(this, args.map{case Sym(name) => name case _ => throw new E("argument should symbol")}, body).toSome
+    case (Sym("begin"), body) =>
+      body.foldLeft[S](SNil){(_, s) => eval(s)}.toSome
     case _ => None
   }
-  def evalApply(func:S, args:Seq[S]):(S, Env) = {
-    val (evaledArgs, env) = evalSeq(args)
-    env.eval(func) match {
-      case (NativeProc(body), e) => (body(evaledArgs), e)
-      case (lambda @ Lambda(closed, params, body), e) =>
-        closed.extend(params, evaledArgs).eval(body) match {
-          case (result, closed) => lambda.env = closed; (result, e)
-        }
+
+  def evalApply(func:S, args:Seq[S]):S = {
+    val evaledArgs = args.map(eval(_))
+    eval(func) match {
+      case NativeProc(body) => body(evaledArgs)
+      case Lambda(closed, params, body) =>
+        closed.updateAll(params, evaledArgs)
+        closed.eval(body)
       case unk => throw new RuntimeException(s"cant apply to ${unk}")
     }
   }
 
-  def evalSeq(exprs:Seq[S]):(Seq[S], Env) = exprs.foldLeft((Seq[S](), this)) { (re, expr) => val (results, env) = re; val (result, newe) = env.eval(expr); (results :+ result, newe) }
-
-  def update(name:String, value:S):Env = new Env(values + (name -> value))
+  def update(name:String, value:S):Unit =
+    this.values = values + (name -> value)
+  def updateAll(names:Seq[String], values:Seq[S]):Unit =
+    names.zip(values).foreach{case (k, v) => update(k, v)}
 
   def extend(names:Seq[String], values:Seq[S]) = new Env(
     names.zip(values).foldLeft(this.values) { (vs, nv) => val (n, v) = nv; vs + (n -> v) }
@@ -105,9 +112,9 @@ class Env(values:Map[String, S]) {
 
 object Env {
   def newGlobal():Env = {
-    new Env(
+    new Env(Map(
       "+" -> NativeProc { case Seq(a, b) => Num(a.as[Num].value + b.as[Num].value) }
-    )
+    ))
   }
 }
 
@@ -129,6 +136,11 @@ abstract sealed class S {
   def isTruthy:Boolean = true
 }
 
+object S {
+  def apply(s:Symbol) = Sym(s.name)
+  def apply(n:Int) = Num(n)
+}
+
 case class Cons(car:S, cdr:S)                            extends S
 case object SNil                                         extends S {
   override def isTruthy = false
@@ -136,7 +148,9 @@ case object SNil                                         extends S {
 case class Num(value:Int)                                extends S
 case class Sym(value:String)                             extends S
 case class NativeProc(body:Seq[S] => S)                  extends S
-case class Lambda(var env:Env, args:Seq[String], body:S) extends S
+case class Lambda(val env:Env, args:Seq[String], body:S) extends S {
+  override def toString() = s"(lambda (${args.mkString(" ")}) ${body}"
+}
 
 object ProperList {
   def unapplySeq(value:S):Option[Seq[S]] = value match {
